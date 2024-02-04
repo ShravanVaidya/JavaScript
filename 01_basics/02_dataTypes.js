f762d142373022e0bbd911f600f00c9aac9352b4
ShravanVaidya <95033999+ShravanVaidya@users.noreply.github.com>
2024-02-04 13:51:01 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+ "use strict"  //treat all code as newer version
+
+ // alert( 4 + 4 )  // we are using nodejs, not browser
+
+ console.log(5+5)
+
+ let name = "Shravan"
+ let age = 20
+ let isLoggedIn = false
+ let state
+
+ //  number = 2^54
+ // bigint
+ // string = ""
+ // boolean 
+ // null = standalone value
+ // undefined
+ // symbol = unique
+
+
+ // object
+
+
+ console.log(typeof null);  // object 
+ console.log(typeof undefined);  // undefined
