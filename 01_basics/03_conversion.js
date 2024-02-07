d716f1b38dee0539c20d5acada58ea7531ffcd14
ShravanVaidya <95033999+ShravanVaidya@users.noreply.github.com>
2024-02-07 13:11:05 +0000

    operators in type conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "50a"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let scoreInNumber = Number(score)
-console.log(typeof scoreInNumber);
-console.log(scoreInNumber);
+//console.log(typeof scoreInNumber);
+//console.log(scoreInNumber);
 
 /*
 
@@ -21,7 +21,7 @@ undefined -> NaN
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 /*
 
@@ -34,5 +34,28 @@ console.log(booleanIsLoggedIn)
 let someNumber = 40
 
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+//console.log(stringSomeNumber);
+//console.log(typeof stringSomeNumber);
+
+
+/*********************************************Operations*********************************************************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let s1 = "hello"
+let s2 = " how are you"
+
+//console.log(2+2);
+
+let s3 = s1+s2
+//console.log(s3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log(+true);
+//console.log(+"");
